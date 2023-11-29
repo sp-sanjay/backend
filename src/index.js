1287ba4e166aa1e9cde467bedff59f29cdf44c4f
portpro-sanjay <sanjay.prajapati@portpro.io>
2023-11-29 18:58:57 +0530

    create utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
-
+import app from './app.js'
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+
+connectDB().then(()=>{
+    const port = process.env.PORT || 8000;
+
+    app.on("Error",(err) => {
+        console.log("Error while connecting server",err)
+        process.exit(1); //For stoping server
+    })
+    app.listen(port,() => {
+        console.log('connection successfully', port)
+    })
+}).catch((err) => {
+    console.log('MongoDB connection Failed !!!', err)
+});
